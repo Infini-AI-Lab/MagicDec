11130763a4fbc942d854eec8d34742c20bb24f5c
jianc99 <jianchen0311@gmail.com>
2024-09-27 21:19:11 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 export ENABLE_INTRA_NODE_COMM=1
 
-torchrun --standalone --nproc_per_node=8 tests/baseline_benchmark.py --compile --rank_group 0 1 2 3 4 5 6 7 --B 45 --prefix_len 100000 --max_len 100096
+torchrun --standalone --nproc_per_node=8 tests/baseline_benchmark.py --compile --rank_group 0 1 2 3 4 5 6 7 --B 44 --prefix_len 100000 --max_len 100096
 
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 45 --prefix_len 100000 --max_len 100096 --gamma 6
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 45 --prefix_len 100000 --max_len 100096 --gamma 7
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 45 --prefix_len 100000 --max_len 100096 --gamma 8
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 45 --prefix_len 100000 --max_len 100096 --gamma 9
+torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 44 --prefix_len 100000 --max_len 100096 --gamma 6
+torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 44 --prefix_len 100000 --max_len 100096 --gamma 7
+torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 44 --prefix_len 100000 --max_len 100096 --gamma 8
+torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 44 --prefix_len 100000 --max_len 100096 --gamma 9
