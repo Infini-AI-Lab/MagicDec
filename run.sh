e70197439483d07b9e2238217c641341d6b99edf
jianc99 <jianchen0311@gmail.com>
2024-09-30 22:57:22 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -79,6 +79,9 @@ torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_grou
 torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 2049 --B 128 --prefix_len 32032 --max_len 32128 --gamma 6
 torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 2049 --B 128 --prefix_len 32032 --max_len 32128 --gamma 7
 
+
+
+
 torchrun --standalone --nproc_per_node=8 tests/baseline_benchmark.py --compile --rank_group 0 1 2 3 4 5 6 7 --B 32 --prefix_len 64032 --max_len 64128
 torchrun --standalone --nproc_per_node=8 tests/baseline_benchmark.py --compile --rank_group 0 1 2 3 4 5 6 7 --B 64 --prefix_len 64032 --max_len 64128
 
@@ -124,16 +127,16 @@ torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_grou
 torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 32 --prefix_len 100000 --max_len 100096 --gamma 7
 torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 4097 --B 32 --prefix_len 100000 --max_len 100096 --gamma 8
 
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 4
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 5
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 6
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 7
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 8
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 4
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 5
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 6
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 7
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 16 --prefix_len 100000 --max_len 100096 --gamma 8
 
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 4
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 5
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 6
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 7
-torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 8
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 4
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 5
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 6
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 7
+# torchrun --standalone --nproc_per_node=8 tests/selfspec_benchmark.py --rank_group 0 1 2 3 4 5 6 7 --benchmark --compile --draft_budget 8193 --B 32 --prefix_len 100000 --max_len 100096 --gamma 8
 
 
